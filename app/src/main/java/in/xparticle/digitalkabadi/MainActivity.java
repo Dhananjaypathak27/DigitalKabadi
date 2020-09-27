07c6b34286ba702a320da2297bb379a2e962052a
Dhananjaypathak27 <43525854+Dhananjaypathak27@users.noreply.github.com>
2020-09-28 01:55:11 +0530

    fragment added and scrollbar view added to ProcedureFragment and content added

--- app/src/main/java/in/xparticle/digitalkabadi/MainActivity.java
+++ app/src/main/java/in/xparticle/digitalkabadi/MainActivity.java
@@ -1,14 +1,97 @@
 package in.xparticle.digitalkabadi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView profilePhoto;
+    BottomNavigationView bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNav = findViewById(R.id.bottomNavigation);
+        profilePhoto = findViewById(R.id.profile);
+
+        intent_function();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragment()).commit();
+
+        profilePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,ProfileActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
+
+    public void intent_function(){
+
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                Fragment selectedFragment = null;
+
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        selectedFragment =new HomeFragment();
+                        break;
+
+                    case R.id.nav_scrape_price:
+                        selectedFragment = new ScrapPriceFragment();
+                        break;
+
+                    case R.id.nav_procedure:
+                        selectedFragment = new ProcedureFragment();
+                        break;
+
+                    case R.id.nav_info:
+                        selectedFragment = new AboutUsFragment();
+                        break;
+                }
+                assert selectedFragment != null;
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        selectedFragment).commit();
+
+                return true;
+            }
+        });
+    }
+
+    boolean doubleBackToExitPressedOnce= false;
+    @Override
+    public void onBackPressed() {
+        if(doubleBackToExitPressedOnce){
+            super.onBackPressed();
+            return;
+        }
+        this.doubleBackToExitPressedOnce =true;
+        Toast.makeText(this, "Press again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+
+            }
+        },2000);
+    }
+
 }
---
+++ app/src/main/java/in/xparticle/digitalkabadi/ScrapPriceFragment.java
@@ -0,0 +1,120 @@
+package in.xparticle.digitalkabadi;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ScrapPriceFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ScrapPriceFragment extends Fragment {
+
+    private ViewTreeObserver.OnScrollChangedListener mOnScrollChangedListener;
+    SwipeRefreshLayout swipeRefreshLayout;
+    WebView webView;
+
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public ScrapPriceFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ScrapPriceFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ScrapPriceFragment newInstance(String param1, String param2) {
+        ScrapPriceFragment fragment = new ScrapPriceFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_scrap_price, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        swipeRefreshLayout = view.findViewById(R.id.scrapePriceSwipeRefresh);
+        webView = view.findViewById(R.id.scrapePriceWebView);
+        scrapWebViewRefreshing();
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                scrapWebViewRefreshing();
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+    }
+    private void scrapWebViewRefreshing(){
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("https://digitalkabadi.in/pickupboy/scrapprice.php");
+//        webView.setWebViewClient(new ScrapePriceFragment.myWebclient());
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        swipeRefreshLayout.getViewTreeObserver().addOnScrollChangedListener(mOnScrollChangedListener =
+                new ViewTreeObserver.OnScrollChangedListener() {
+                    @Override
+                    public void onScrollChanged() {
+                        if (webView.getScrollY() == 0)
+                            swipeRefreshLayout.setEnabled(true);
+                        else
+                            swipeRefreshLayout.setEnabled(false);
+
+                    }
+                });
+    }
+    @Override
+    public void onStop() {
+        swipeRefreshLayout.getViewTreeObserver().removeOnScrollChangedListener(mOnScrollChangedListener);
+        super.onStop();
+    }
+
+}
